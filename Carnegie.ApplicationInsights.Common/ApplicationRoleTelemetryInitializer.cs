c40621da5cf0a9cf5e03565373ae2a0e4be7ce87
Emil Åström <emiast@carnegie.se>
2020-10-20 09:12:50 +0200

    Set instance name on events
    Avoids mixing "tsse109" and "tsse109.carnnet.com" for different events.

--- Carnegie.ApplicationInsights.Common/ApplicationRoleTelemetryInitializer.cs
+++ Carnegie.ApplicationInsights.Common/ApplicationRoleTelemetryInitializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.Extensibility;
 
@@ -7,13 +8,20 @@ namespace Carnegie.ApplicationInsights.Common
     public class ApplicationRoleTelemetryInitializer : ITelemetryInitializer
     {
         private string _roleName;
+        private string _instanceName;
 
         public void Initialize(ITelemetry telemetry)
         {
             if (string.IsNullOrEmpty(telemetry.Context.Cloud.RoleName))
             {
+                // Set role name to service or worker name so that events more easily can be associated to the correct components.
+                // Also necessary for the application map to work.
                 _roleName ??= (Assembly.GetEntryAssembly() ?? Assembly.GetExecutingAssembly()).GetName().Name;
                 telemetry.Context.Cloud.RoleName = _roleName;
+
+                // Explicitly set instance name to avoid mixing computer name ("TSSE109") and network names ("TSSE109.carnnet.com") in events.
+                _instanceName ??= Environment.MachineName;
+                telemetry.Context.Cloud.RoleInstance = _instanceName;
             }
         }
     }
