af944da2afb2ffd5b713a81ab717767c5fb34618
Emil Åström <emiast@carnegie.se>
2020-11-15 15:57:36 +0100

    Created a new project with for Serilog integration with Application Insights

---
+++ Carnegie.ApplicationInsights.Logging/ApplicationInsightsLoggingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Carnegie.ApplicationInsights.Common;
+using Microsoft.ApplicationInsights.Extensibility;
+using Serilog;
+using Serilog.Configuration;
+
+namespace Carnegie.ApplicationInsights.Logging
+{
+    public static class ApplicationInsightsLoggingExtensions
+    {
+        //public static void ApplicationInsightsSink(this LoggerSinkConfiguration writeTo)
+        //{
+        //    writeTo.ApplicationInsights(TelemetryConfiguration.Active, TelemetryConverter.Traces);
+        //}
+        
+        public static void ApplicationInsightsSink(this LoggerSinkConfiguration writeTo, string instrumentationKey)
+        {
+            var telemetryConfiguration = TelemetryConfiguration.CreateDefault();
+            telemetryConfiguration.InstrumentationKey = instrumentationKey;
+            telemetryConfiguration.TelemetryInitializers.Add(new ApplicationRoleTelemetryInitializer());
+
+            writeTo.ApplicationInsights(telemetryConfiguration, TelemetryConverter.Traces);
+        }
+    }
+}
