3306e3c89437178317bb240072edd4f02a699b15
Mats Andersson <matand@carnegie.se>
2020-11-16 15:10:23 +0100

    Adding Carnegie.ApplicationInsights.MassTransit

---
+++ Carnegie.ApplicationInsights.MassTransit/Filters/ApplicationInsightsConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Carnegie.ApplicationInsights.MassTransit.Filters
+{
+	public class ApplicationInsightsConstants
+	{
+		public const string RootId = "RootId";
+		public const string ParentId = "ParentId";
+		public const string TelemetryType = "Masstransit";
+		public const string Parent = "Parent";
+
+	}
+}
