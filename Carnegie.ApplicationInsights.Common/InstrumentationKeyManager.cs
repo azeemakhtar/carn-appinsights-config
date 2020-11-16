4da3f1ea32a24526d254107ac46f5fcc04d1b0a2
Emil Åström <emiast@carnegie.se>
2020-11-16 11:11:31 +0100

    Enable App insights for Devopment environment
    The events are viewable in Visual Studio but are not sent to an Application Insights resource in the cloud.

--- Carnegie.ApplicationInsights.Common/InstrumentationKeyManager.cs
+++ Carnegie.ApplicationInsights.Common/InstrumentationKeyManager.cs
@@ -14,7 +14,7 @@ namespace Carnegie.ApplicationInsights.Common
             switch (environmentName.ToUpper())
             {
                 case "DEVELOPMENT":
-                    return null;
+                    return Guid.Empty.ToString(); // So that Application Insights is enabled and the events are viewable in Visual Studio
                 case "TEST":
                     return "d518983e-6365-4eb6-91f8-d89c443b124a";
                 case "PREPROD":
