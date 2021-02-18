5b64b12ba25f09367551928a66baf6f390c3ff6c
Emil Åström <emiast@carnegie.se>
2021-02-18 12:37:23 +0100

    Added instrumentation keys for test1, preprod1, preprod and prod environments

--- Carnegie.ApplicationInsights.Common/InstrumentationKeyManager.cs
+++ Carnegie.ApplicationInsights.Common/InstrumentationKeyManager.cs
@@ -24,11 +24,16 @@ namespace Carnegie.ApplicationInsights.Common
                 case "DEVELOPMENT":
                     return EmptyKey();
                 case "TEST":
-                    return "d518983e-6365-4eb6-91f8-d89c443b124a";
+                    return "d518983e-6365-4eb6-91f8-d89c443b124a"; // Old
+                    //return "2c0b4ed7-0011-444d-97b6-3206a5da2953"; // New, we´ll switch when all developers have access
+                case "TEST1":
+                    return "5e758f1c-66ac-4fc7-a630-ea8e881b1986";
                 case "PREPROD":
-                    return null;
+                    return "74f0e7d0-6cf3-4513-a35e-ca4c1ebd2663";
+                case "PREPROD1":
+                    return "aedeaa4a-3264-4994-8205-13bd7721da72";
                 case "PRODUCTION":
-                    return null;
+                    return "0325c234-bedb-4d27-8037-f0fef59d75b4";
                 default:
                     throw new ArgumentException($"Unknown environment: {environmentName}");
             }
